152dfe17c8821d91d5311db2769d9051b9571c4d
Rowan Simmons <59707215+rowsimms42@users.noreply.github.com>
2021-01-08 15:07:01 -0500

    clean up

--- main.cpp
+++ main.cpp
@@ -7,24 +7,24 @@
 #include <SDL2/SDL_mixer.h>
 
 int main(int argc, char* argv[]){
-  SDL_Window *window;                    // Declare a pointer
-  SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+  SDL_Window *window;                    //window pointer
+  SDL_Init(SDL_INIT_VIDEO);              //initialize SDL2
 
-  // Create an application window with the following settings:
+  //create an application window
   window = SDL_CreateWindow(
-    "Lethal Larry",                  // window title
-    SDL_WINDOWPOS_UNDEFINED,           // initial x position
-    SDL_WINDOWPOS_UNDEFINED,           // initial y position
-    640,                               // width, in pixels
-    480,                               // height, in pixels
-    SDL_WINDOW_OPENGL                  // flags - see below
+    "Lethal Larry",                  //window title
+    SDL_WINDOWPOS_UNDEFINED,         //initial x position
+    SDL_WINDOWPOS_UNDEFINED,         //initial y position
+    640,                             //width, in pixels
+    480,                             //height, in pixels
+    SDL_WINDOW_OPENGL                //flags - see below
     );
-    // Check that the window was successfully created
+    //check if window was successfully created
     if (window == NULL){
       printf("Could not create window: %s\n", SDL_GetError());
       return 1;
     }
-    // The window is open: could enter program loop here (see SDL_PollEvent())
+    //window is open: could enter program loop here (SDL_PollEvent())
     SDL_Event e;
     bool quit = false;
     while (!quit){
@@ -40,11 +40,7 @@ int main(int argc, char* argv[]){
         }
       }
     }
-
-    // Close and destroy the window
-    SDL_DestroyWindow(window);
-
-    // Clean up
-    SDL_Quit();
+    SDL_DestroyWindow(window); //close window
+    SDL_Quit(); //quit
     return 0;
 }
